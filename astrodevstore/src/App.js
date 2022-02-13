52f14163dc39d36ac9d7d5a1d6d90015f237bb16
taypolitano <taypolitano@yahoo.com>
2022-02-13 17:19:01 -0300

    fiz a primeira parte

---
+++ astrodevstore/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './component/navbar';
+import Home from './component/home';
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Home/>
+
+   
+   
+   
+   </>
+  );
+}
+
+export default App;
